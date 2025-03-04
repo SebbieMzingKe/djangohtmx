b5707c2f8a1bca827dec912793a29fe78c6620d2
SebbieMzingKe <sebbievilar2@gmail.com>
2025-03-04 16:14:36 +0300

    initial commit

---
+++ contacts/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='contacts'
+        )
+
+
+    class Meta:
+        unique_together = ('name', 'email')
+        def __str__(self):
+            return f"{self.name} <{self.name}>"
