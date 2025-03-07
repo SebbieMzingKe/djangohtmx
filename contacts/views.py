8a87921df5f5e52237aba93e9a16106b43b45994
SebbieMzingKe <sebbievilar2@gmail.com>
2025-03-07 10:18:36 +0300

    update (chore): display contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
-    context = {}
+    contacts = request.user.Contact.all().order_by('-created_at')
+    context = {contacts: contacts}
+    print(contacts)
     return render(request, "contact.html", context)
